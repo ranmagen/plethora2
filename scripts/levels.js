8efcd16e0247fdd1a5b3ccdae31836baed31f597
smadarr <smadar.rotem@gmail.com>
2017-02-21 10:57:31 +0200

    Add files via upload

--- scripts/levels.js
+++ scripts/levels.js
@@ -7,15 +7,15 @@ var config = {
 };
 
 $(function () {
-    var canvas = document.querySelector('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.canvas.width = window.innerWidth;//$("#sidebar").offset().left;
-    ctx.canvas.height = window.innerHeight - 60;//$("#header").height();
-    ctx.fillStyle = '#00031a';
-    //ctx.strokeStyle = '00031a';
-    var container = { x: 60, y: 0, width: ctx.canvas.width - 130, height: ctx.canvas.height - 60 };
-    ctx.fillStyle = '#00031a';
-    ctx.fillRect(container.x, container.y, container.width, container.height);
+    // var canvas = document.querySelector('canvas');
+    // var ctx = canvas.getContext('2d');
+    // ctx.canvas.width = window.innerWidth;
+    // ctx.canvas.height = window.innerHeight - 60;
+    // ctx.fillStyle = '#00031a';
+    // //ctx.strokeStyle = '00031a';
+    // var container = { x: 60, y: 0, width: ctx.canvas.width - 130, height: ctx.canvas.height - 60 };
+    // ctx.fillStyle = '#00031a';
+    // ctx.fillRect(container.x, container.y, container.width, container.height);
 
     firebase.initializeApp(config);
     var dbRef = firebase.database().ref('levels');
@@ -27,7 +27,7 @@ $(function () {
         for (var i = 0; i < levelsCnt; i++) {
            // var levelName = data.child(i).val().name;
 
-            if (i % 5 == 0) {
+            if (i % 4 == 0) {
                 row = i;
                 $('<div></div>').attr('id', 'row' + i).addClass('row').appendTo('#levels-container');
             }
@@ -36,7 +36,7 @@ $(function () {
             //$('<img src="../images.levels/level"' + lvl + '.png />')
             //$('<div><img src="images/levels/level1.png"/></div>').attr('id', 'level_' + i).data('number', i).addClass('level').text(data.child(i).val().name).click(function () {
             //    window.location.href = "index.html?level=" + $(this).data('number');
-            $('<div><img class="level-img" src="images/levels/level' + lvl + '.png"/><p>' + data.child(i).val().name + '</p></div>').attr('id',
+            $('<div><img class="level-img" src="images/levels/level' + lvl + '.png"/><p class="level-name">' + data.child(i).val().name + '</p></div>').attr('id',
                 'level_' + i).data('number', i).addClass('level').click(function () {
                 window.location.href = "index.html?level=" + $(this).data('number');
             }).appendTo('#row' + row);
