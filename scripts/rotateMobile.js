1209ace3c50fdd60374bcb7860b36ef59deda5e1
smadarr <smadar.rotem@gmail.com>
2017-03-25 19:03:26 +0300

    Add files via upload

---
+++ scripts/rotateMobile.js
@@ -0,0 +1,15 @@
+$(function(){});
+
+   $(document).on("pagecreate",function(event){ 
+        $(window).on("orientationchange",function(){
+            if(window.orientation == 0) //profile
+            {
+                $('body').prepend('<img id="rotate-img" src="images/rotate.png" class="centered" />');
+            }
+            else //landscape
+            {
+              $("#rotate-img").hide();
+            }
+        });                   
+    });
+
