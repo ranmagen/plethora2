712010bb5a3454df97ef10300426e0625237a2e7
smadarr <smadar.rotem@gmail.com>
2017-03-26 20:19:50 +0300

    Add files via upload

--- scripts/rotateMobile.js
+++ scripts/rotateMobile.js
@@ -4,17 +4,16 @@ $(function () {
 
 function OrientationChange()
 {
-    alert("OrientationChange");
        $(document).on("pagecreate",function(event){ 
         $(window).on("orientationchange",function(){
             if(window.orientation == 0) //profile
             {
-                alert("profile");
+                //alert("profile");
                 $("#overlay").show();
             }
             else //landscape
             {
-                alert("landscape");
+               // alert("landscape");
                 $("#overlay").hide();
             }
         });                   
